352171ced4e45e8ed801bdb9d28c04d20fabce15
ZahraaTazmeen <zahraa.tazmeen@gmail.com>
2023-10-16 22:08:58 -0400

    hi

--- src/hw11Abstraction/ColumbiaUniversity.java
+++ src/hw11Abstraction/ColumbiaUniversity.java
@@ -12,6 +12,84 @@ public class ColumbiaUniversity extends MedicalSchool implements University, Voc
 
 	public ColumbiaUniversity() {
 	}
+
+	@Override
+	public void commonRoom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void laboratory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void languageClub() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emergencyRoom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surgeryRoom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cafeteria() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lawInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vocationalInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void classSize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playGround() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void teacher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void anatomyLab() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hygiene() {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
 /*
