ace3c74bcd3cbdbf8991c31cce91248bebca8c83
ZahraaTazmeen <zahraa.tazmeen@gmail.com>
2024-03-02 13:40:19 -0500

    new

---
+++ src/hw18UseOfLoop/LoopUse.java
@@ -0,0 +1,14 @@
+package hw18UseOfLoop;
+
+import java.util.Iterator;
+
+public class LoopUse {
+
+	public static void main(String[] args) {
+for (int i = -20;i<=50;i+=1) {
+	if (i % 2 == 1 || i % 2 == -1) {
+		System.out.println("Odd Number:"+i);
+			}
+		}
+	}
+}
