1e4f890f989beeeaf1080dc2a4879e19ad1a655b
ZahraaTazmeen <zahraa.tazmeen@gmail.com>
2024-04-06 15:21:32 -0400

    homework

--- src/hw19CodingChallenge01/characterOneByOneVerticallyButReverse.java
+++ src/hw19CodingChallenge01/characterOneByOneVerticallyButReverse.java
@@ -4,7 +4,7 @@ package hw19CodingChallenge01;
 public class characterOneByOneVerticallyButReverse {
 	public static void main(String[] args) {
 		String s = "Zahraa Tazmeen";
-				for(int i = s.length(); i > 0 ;i--){
+				for(int i=s.length()-1; i >= 0; i--){
 				System.out.println(s.charAt(i)); 
 		
 				}
